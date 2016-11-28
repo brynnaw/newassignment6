f2a4fcefd3b674523d436261757897423dfee6f7
Erin Gallagher <ep74468@gmail.com>
2016-11-28 21:50:52 +0000

    modified build-presentation.sh

--- build-presentation.sh
+++ build-presentation.sh
@@ -6,3 +6,4 @@ TRANSITION=slide
 THEME=black
 
 pandoc -t revealjs --template=$TEMPLATE -s --variable theme="$THEME" --variable transition="$TRANSITION" --variable revealjs-url="./" $INPUT.md -o index.html
+
